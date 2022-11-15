eb2a72896cf56a844a91d8684ba7175c9871f62e
belkinmex <118251639+belkinmex@users.noreply.github.com>
2022-11-15 13:08:46 +0500

    test

---
+++ Calc.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) throws IOException {
+        String itogStr;
+        int itog = 0;
+        int x;
+        int y;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("введите выражение");
+        var str = in.nextLine();
+        String[] strArrays = str.split(" ");   // переводит строку в массив строк
+
+        if (strArrays.length == 3) { //если длина ровна 3м
+
+            boolean isRome = Number.isRome(strArrays[0]) && Number.isRome(strArrays[2]);
+
+            if (isRome) {
+                x = Number.valueOf(strArrays[0]).getTranslations();
+                y = Number.valueOf(strArrays[2]).getTranslations();
+            } else {
+                try {
+                    x = Integer.parseInt(strArrays[0]);
+                    y = Integer.parseInt(strArrays[2]);
+                } catch (NumberFormatException e) {
+                    throw new IOException("не верный ввод");
+                }
+            }
+
+            if (x > 0 && x <= 10 || x < 0 && x >= (-10)) {
+                if (y > 0 && y <= 10 || y < 0 && y >= (-10)) {
+
+                    switch (strArrays[1]) {
+                        case ("+") -> itog = x + y;
+                        case ("*") -> itog = x * y;
+                        case ("/") -> itog = x / y;
+                        case ("-") -> itog = x - y;
+                        default -> {
+                            System.out.println("некорректный ввод знака");
+                            throw new IOException();
+                        }
+                    }
+                }
+            } else throw new IOException("некорректный ввод");
+
+            if (isRome) {
+                if (itog >= 0) {
+                    itogStr = Number.toRome(itog);
+                    System.out.println(itogStr);
+                } else throw new IOException("результат в римской СС может быть только положительным");
+            } else System.out.println(itog);
+
+        } else throw new IOException("некорректный ввод выражения ");
+    }
+}
+
+
+
---
+++ Number.java
@@ -0,0 +1,34 @@
+public enum Number {
+    C(100),XC(90),L(50),XL(40),X(10),IX(9),VIII(8),VII(7),VI(6),V(5),IV(4),III(3),II(2),I(1),O(0);
+    static String itogRome="";
+    private final int translations;
+    Number(int translations) {
+        this.translations=translations;
+    }
+
+    public static boolean isRome(String strArray) {
+    for (Number x: values()){
+        if (x.name().equals(strArray)){
+            return true;
+        }
+    }
+    return false;
+    }
+
+
+
+    public int getTranslations() {
+        return translations;
+    }
+    public static String toRome(int itog) {
+      if (itog> 0) {
+            for (Number i : values()) {
+                while (i.translations <= itog) {
+                    itogRome = itogRome + i;
+                    itog = itog - i.translations;
+                }
+            }
+      } else {itogRome = itogRome + O;}
+            return itogRome;
+    }
+}
